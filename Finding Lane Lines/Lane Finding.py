5d5bb422fc62bad2a3f86c012b34477bc201a51a
zcfang <zhongcfang@gmail.com>
2016-09-04 15:35:51 -0700

    v0.2: Eliminate background before finding road lanes by comparing color threshold

--- Finding Lane Lines/Lane Finding.py
+++ Finding Lane Lines/Lane Finding.py
@@ -41,15 +41,31 @@ ySize = image.shape[0]
 xSize = image.shape[1]
 colorSelect = np.copy(image)
 
-# Loop through each pixel and compare it with threshold value
+# Defining lane boundaries 
+leftBottom = [100,539]
+rightBottom = [859, 539]
+apex = [475, 320]
+
+# Creating region of interest 
+leftFit = np.polyfit((leftBottom[0], apex[0]), (leftBottom[1], apex[1]), 1)
+rightFit = np.polyfit((rightBottom[0], apex[0]), (rightBottom[1], apex[1]), 1)
+bottomFit = np.polyfit((leftBottom[0], rightBottom[0]), (leftBottom[1], rightBottom[1]), 1)
+
 for x in range(xSize):
 	for y in range(ySize):
-		red = image[y, x, 0]
-		green = image[y, x, 1]
-		blue = image[y, x, 2]
-
-		if (red < RED_THRESHOLD or green < GREEN_THRESHOLD or blue < BLUE_THRESHOLD):
+# Loop through each pixel and create region of interest
+		if (y < leftFit[0]*x + leftFit[1] or 
+			y < rightFit[0]*x + rightFit[1] or 
+			y > bottomFit[0]*x + bottomFit[1]):
 			colorSelect[y, x, :] = [0, 0, 0]
+# Loop through each pixel and compare it with threshold value
+		else:
+			red = image[y, x, 0]
+			green = image[y, x, 1]
+			blue = image[y, x, 2]
+
+			if (red < RED_THRESHOLD or green < GREEN_THRESHOLD or blue < BLUE_THRESHOLD):
+				colorSelect[y, x, :] = [0, 0, 0]
 
 plt.imshow(colorSelect)
 plt.show()
